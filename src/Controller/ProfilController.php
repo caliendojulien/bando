ea1e5dcb9b1f743f57bf075da4111ba5a1339e1a
BGG44 <brunogar@laposte.net>
2023-02-14 20:47:49 +0100

    J'ai avancé sur la gestion du profil et le téléchargement de la photo

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,12 +2,11 @@
 
 namespace App\Controller;
 
-use App\Entity\Campus;
-use App\Entity\Stagiaire;
 use App\Form\ProfilType;
+use App\Repository\StagiaireRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,22 +14,22 @@ class ProfilController extends AbstractController
 {
 
     #[Route('/profil', name: 'profil_modif')]
-    public function index(EntityManagerInterface $em, Request $request): Response
+    public function index(EntityManagerInterface $em, Request $request, StagiaireRepository $stagiaireRepository): Response
     {
-        $stagiaires = new Stagiaire();
         $stagiaires = $this->getUser();
-        $campus = $em->getRepository(Campus::class)->findAll();
         $profilForm = $this->createForm(ProfilType::class, $stagiaires);
-
         $profilForm->handleRequest($request);
 
-        if ($profilForm->isSubmitted() && $profilForm->isValid()) {
+        if ($profilForm->isSubmitted()) {
+            dump("test");
             $em->persist($stagiaires);
             $em->flush();
+
         }
 
         return $this->render('profil/modif.html.twig', [
             'profilForm' => $profilForm,
+            'url' => $profilForm->get('url_photo')->getData()
         ]);
     }
 }
--- src/Form/ProfilType.php
+++ src/Form/ProfilType.php
@@ -2,21 +2,15 @@
 
 namespace App\Form;
 
-use App\Controller\ProfilController;
-use App\Entity\Campus;
 use App\Entity\Stagiaire;
-use App\Repository\CampusRepository;
-use Doctrine\Bundle\DoctrineBundle\Registry;
-use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\ChoiceList\ChoiceList;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -24,21 +18,21 @@ class ProfilType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+
         $builder
-            ->add('prenom')
-            ->add('nom')
-            ->add('telephone')
-            ->add('email')
+            ->add('prenom', null, ['attr' => ['class' => 'profilPrenom'],])
+            ->add('nom', null, ['attr' => ['class' => 'profilNom'],])
+            ->add('telephone', TelType::class, ['attr' => ['class' => 'profilPrenom'],])
+            ->add('email', EmailType::class, ['attr' => ['class' => 'profilEmail'],])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'first_options' => ['label' => 'Mot de passe', 'hash_property_path' => 'password'],
                 'second_options' => ['label' => 'Confirmation mot de passe'],
                 'mapped' => false,
             ])
-            ->add('campus')
-            ->add('urlPhoto')
+            ->add('campus', null, ['attr' => ['class' => 'profilCampus'],])
+            ->add('url_photo', HiddenType::class, ['attr' => ['visible' => 'hidden'],])
             ->add('photo_download', FileType::class, ['mapped' => false])
-            //->add('campus')
             ->add('Envoyer', SubmitType::class);
     }
 
