fc9e3dc674f68e8ad8b0238031fb38ec0e7195b9
BGG44 <brunogar@laposte.net>
2023-02-16 20:07:03 +0100

    Mise en place repertoire command pour la commande UpdateEtatCommand

--- src/Command/UpdateEtatCommand.php
+++ src/Command/UpdateEtatCommand.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace App\Services;
+namespace App\Command;
 
 use App\Repository\SortieRepository;
+use DateTime;
+use DateTimeZone;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use DateTime;
-use DateTimeZone;
 
 #[AsCommand(name: 'app:update-etat')]
 class UpdateEtatCommand extends Command
